603b32b51e9e0e387ba18f607ecc763c9d3c8664
Lucas Peter Aarestrup Wenzel Høegh Kyhl <luca609a@edu.campusvejle.dk>
2020-06-19 10:16:37 +0200

    Setup basic javascript

--- om-os.js
+++ om-os.js
@@ -1,3 +1,27 @@
 "use strict";
 
-console.log("Test")
+console.log("Test");
+
+const featured = document.querySelector('#featured > picture > img');
+const featuredSelection = document.querySelectorAll('#om-os > div > picture > img');
+
+const featuredH4 = document.querySelector('.featured-text > h4');
+
+
+// console.log(featuredSelection);
+
+// featuredSelection.addEventListener('click', function(imageSelection) {
+//     console.log('It got clicked.');
+//     featured.src = imageSelection.target.src;
+// });
+
+
+function imageSelection() {
+    console.log('It got clicked.');
+    featured.src = imageSelection.target.src;
+};
+
+featuredSelection.forEach(function(featuredSelect) {
+    console.log(featuredSelect);
+    featuredSelect.addEventListener('click', imageSelection)
+});
